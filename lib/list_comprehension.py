9605b595ed9eeddc673fa334758966d658e76dd3
DianaTuei <memotuei@gmail.com>
2024-03-03 16:38:08 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return_evens= []
+    for num in num_list:
+        if(num%2 == 0):
+            return_evens.append(num)
+    return return_evens
+return_evens([1,10,2])
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = []
+    for sentence in sentence_list:
+        exclamation_list.append(f"{sentence}!")
+    print(exclamation_list)
+    return exclamation_list
+make_exclamation(["I like computers", "I require coffee", "Live long and prosper"])
